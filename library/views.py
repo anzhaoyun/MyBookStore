1a6bb0d10535924f812834741883ac9d4c48dd92
anzhaoyun <2014243078@qq.com>
2021-07-21 17:37:12 +0800

    视图

--- library/views.py
+++ library/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render,HttpResponse
-
+from library import models
 # Create your views here.
 
 def add_library(request):
-
-    pass
+    data = request.POST
+    print(data)
+    # library = models.Library(name='图书馆2',address='北京',e_mail='2014243078@qq.com',
+    #     tel_number='13569830431',img='https://tse1-mm.cn.bing.net/th?id=OIP-C.dPt_kwI23ck0zG3TquTZJAHaFj&w=203&h=160&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2')
+    # library.save()
+    return HttpResponse("<p>添加成功！</p>")
 
 def get_library(request):
 
